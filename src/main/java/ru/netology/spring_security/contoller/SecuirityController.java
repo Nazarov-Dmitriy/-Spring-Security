dd2cdbd13fc8edaecc214c26ed813c2abdb833be
Dmitriy Nazarov <difat1@inbox.ru>
2023-12-03 04:58:02 +0300

    initial commit

--- src/main/java/ru/netology/spring_security/contoller/SecuirityController.java
+++ src/main/java/ru/netology/spring_security/contoller/SecuirityController.java
@@ -1,6 +1,12 @@
 package ru.netology.spring_security.contoller;
 
+
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,17 +17,29 @@ public class SecuirityController {
         return "hello";
     }
 
+    @Secured({"ROLE_READ"})
     @GetMapping("/read")
     public String read() {
         return "read";
     }
 
+    @RolesAllowed({"WRITE"})
     @GetMapping("/write")
     public String write() {
         return "write";
     }
 
+    @PostAuthorize("hasAnyAuthority('DELETE')")
+    @GetMapping("/delete")
+    public String delete() {
+        return "delete";
+    }
 
 
+    @GetMapping("/user")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String getMyRoles(@RequestParam(name = "username") String username) {
+        return username;
+    }
 }
 
--- src/main/java/ru/netology/spring_security/security/SecurityConfig.java
+++ src/main/java/ru/netology/spring_security/security/SecurityConfig.java
@@ -2,37 +2,23 @@ package ru.netology.spring_security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(
-                        (authorize) -> authorize
-                                .requestMatchers("hello").permitAll()
-                                .requestMatchers("/read").hasAuthority("read")
-                                .requestMatchers("/write").hasAuthority("write")
-                                .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
-        return http.build();
-    }
 
     @Bean
     public UserDetailsService userDetailsService() throws Exception {
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").authorities("read").build());
-        manager.createUser(users.username("user2").password("password").roles("USER").authorities("read", "write").build());
-        manager.createUser(users.username("user3").password("password").roles("USER").authorities("write").build());
+        manager.createUser(users.username("user").password("password").roles("READ").build());
+        manager.createUser(users.username("user2").password("password").roles("WRITE").build());
+        manager.createUser(users.username("user3").password("password").authorities("DELETE").build());
         return manager;
     }
 }
